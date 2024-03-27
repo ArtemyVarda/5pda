2fcff9ad2082669563db783c9b6ff7ccc78ce7f1
towel17 <towel1703@inbox.ru>
2024-03-27 20:46:49 +0400

    new add entity and rep

--- entity/Users.php
+++ entity/Users.php
@@ -2,6 +2,7 @@
 
 namespace app\entity;
 
+use app\repository\UsersRepository;
 use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
 
@@ -18,16 +19,29 @@ class Users extends ActiveRecord implements IdentityInterface
 
     public static function findIdentity($id)
     {
+        return new static(UsersRepository::getUserbyId($id));
+    }
+
+    public function getId()
+    {
+        return $this->id;
+
+    }
+
+    public function findUserByLogin($login)
+    {
+        return new static(UsersRepository::getUserByLogin($login));
+    }
+
+    public function validatePassword($password)
+    {
+        return password_verify($password, $this->password);
     }
 
     public static function findIdentityByAccessToken($token, $type = null)
     {
     }
 
-    public function getId()
-    {
-
-    }
 
     public function getAuthKey()
     {
